cb890d754519704d307d69aad7d9e92a0a6cd377
muzimasoom <129160250+muzimasoom@users.noreply.github.com>
2023-11-18 13:59:19 +0500

    Update PARIZAD.py
    rm -rf Muzi
    git clone https://github.com/muzimasoom/PARIZAD
    cd PARIZAD
    python PARIZAD.py

--- PARIZAD.py
+++ PARIZAD.py
@@ -1430,7 +1430,7 @@ def free(idf,pwv):
 
 
 
-			ses.headers.update({"authority': 'p.facebook.com',
+			ses.headers.update({"Host": 'p.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'method': 'GET',
 	'scheme': 'https',
@@ -1461,7 +1461,7 @@ def free(idf,pwv):
 
 
 
-			ses.headers.update({"authority': 'p.facebook.com',
+			ses.headers.update({"Host": 'p.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'method': 'GET',
 	'scheme': 'https',
